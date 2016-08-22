d4fa8ecd616fdb8cee89068d372383b06f3e8c2f
ilokhov <ilokhov.dev@gmail.com>
2016-08-22 23:02:56 +0200

    change indentation

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
 var app = angular.module('app', ['ngAnimate']);
 
 function ManageCtrl($scope) {
-	$scope.projects = [];
-	$scope.orig = angular.copy($scope.projects);
-	$scope.projectPlaceholder = 'Project name';
+  $scope.projects = [];
+  $scope.orig = angular.copy($scope.projects);
+  $scope.projectPlaceholder = 'Project name';
 
-	// app functions
-	$scope.reset = function() {
-		$scope.projects = angular.copy($scope.orig);
-	};
+  // app functions
+  $scope.reset = function() {
+    $scope.projects = angular.copy($scope.orig);
+  };
 
-	// project specific functions
-	$scope.add = function() {
-		if ($scope.projectName) {
-			$scope.projects.push({name:$scope.projectName});
-			$scope.projectName = '';
-			$scope.projectPlaceholder = 'Project name';
-		}
-		else {
-			$scope.projectPlaceholder = 'Title required';
-		}
-	};
+  // project specific functions
+  $scope.add = function() {
+    if ($scope.projectName) {
+      $scope.projects.push({name:$scope.projectName});
+      $scope.projectName = '';
+      $scope.projectPlaceholder = 'Project name';
+    }
+    else {
+      $scope.projectPlaceholder = 'Title required';
+    }
+  };
 
-	$scope.remove = function(index){
-		$scope.projects.splice(index, 1);
-	};
+  $scope.remove = function(index){
+    $scope.projects.splice(index, 1);
+  };
 
 }
 
 function TaskCtrl($scope) {
-	$scope.tasks = [];
-	$scope.taskPlaceholder = 'Task name';
+  $scope.tasks = [];
+  $scope.taskPlaceholder = 'Task name';
 
-	$scope.add = function() {
-		if ($scope.taskName) {
-			$scope.tasks.push({name:$scope.taskName});
-			$scope.taskName = '';
-			$scope.taskPlaceholder = 'Task name';
-		}
-		else {
-			$scope.taskPlaceholder = 'Title required';
-		}
+  $scope.add = function() {
+    if ($scope.taskName) {
+      $scope.tasks.push({name:$scope.taskName});
+      $scope.taskName = '';
+      $scope.taskPlaceholder = 'Task name';
+    }
+    else {
+      $scope.taskPlaceholder = 'Title required';
+    }
 
-	};
+  };
 
-	$scope.remove = function(index){
-		$scope.tasks.splice(index, 1);
-	};
+  $scope.remove = function(index){
+    $scope.tasks.splice(index, 1);
+  };
 }
